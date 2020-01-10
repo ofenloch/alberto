d57b7bdca182061addbd840e64faeb3d66497ed3
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-10 22:45:20 +0100

    use very basic excel sheet in InputComponent

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Cell } from '../excel/cell';
+import { Row } from '../excel/row';
+import { Sheet } from '../excel/sheet';
+
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
+
 export class InputComponent implements OnInit {
 
-  constructor() { }
+  sheet: Sheet;
+  rowIndices: Array<number>;
+  cellIndices: Array<number>;
 
-  ngOnInit() {
+  constructor() {
   }
 
-}
+  get NRows(): number {
+    return this.sheet.NRows;
+  }
+
+  get Rows(): Array<Row> {
+    return this.sheet.Rows;
+  }
+
+  Row(index: number): Row {
+    return this.sheet.Row(index);
+  }
+
+  Cell(rowIdx: number, cellIdx: number): Cell {
+    return this.sheet.Cell(rowIdx, cellIdx);
+  }
+
+  ngOnInit() {
+    this.sheet = new Sheet(5, 8);
+    this.rowIndices = new Array<number>(this.sheet.NRows);
+    for (let i = 0; i < this.sheet.NRows; i++) {
+      this.rowIndices[i] = i;
+    }
+    this.cellIndices = new Array<number>(this.sheet.NMaxCells);
+    for (let j = 0; j < this.sheet.NMaxCells; j++) {
+      this.cellIndices[j] = j;
+    }
+  }
+
+} // export class InputComponent implements OnInit
