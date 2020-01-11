08ccf4224d84b3fafffdda659a85d48961333151
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-11 16:12:02 +0100

    create a png image for README.md

--- src/assets/logo2ico.sh
+++ src/assets/logo2ico.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+FILENAME=alberto-logo.svg
+BASENAME=`basename ${FILENAME} .svg`
+
 # it seems, the favicon has to be located in ${workspaceFolder}/src/
-convert -density 384 -background transparent alberto-logo.svg -define icon:auto-resize -colors 256 ../favicon.ico
+convert -density 384 -background transparent ${FILENAME} -define icon:auto-resize -colors 256 ../favicon.ico
+
+inkscape -z -e ${BASENAME}-100x100px.png -h 100 -w 100 ${FILENAME};
