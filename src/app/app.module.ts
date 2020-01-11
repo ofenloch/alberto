ab353cc60f37069e2bbd02ff50dcf491dbf96320
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-11 09:39:27 +0100

    use SheetJSComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
+import { SheetJSComponent } from './excel/sheetjs.component';
+
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
+    SheetJSComponent,
     AppComponent,
     InputComponent
   ],
