7ad41f65d770b0d2a328530be42f26425e40984a
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-01-12 15:33:56 +0100

    implment WorkbookComponent

--- src/app/workbook/workbook.component.ts
+++ src/app/workbook/workbook.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SheetJS, AOA } from '../excel/sheetjs';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-workbook',
@@ -7,9 +9,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkbookComponent implements OnInit {
 
+  fileToUpload: File = null;
+  rABS: any = null;
+  domrabs: any = null;
+  sheetJS: SheetJS = null;
+  workbook: XLSX.WorkBook = null;
+  format = 'CSV';
+  wbText = '';
+  wbHtml = '';
+
   constructor() { }
+  data: AOA = [[1, 2], [3, 4]];
+  wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+  fileName = 'SheetJS.xlsx';
+
+  onFileChange(evt: any) {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) {
+      throw new Error('Cannot use multiple files');
+    }
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      this.workbook = XLSX.read(bstr, { type: 'binary' });
+      /* grab first sheet */
+      const wsname: string = this.workbook.SheetNames[0];
+      const ws: XLSX.WorkSheet = this.workbook.Sheets[wsname];
+      /* save data */
+      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+      this.sheetJS = new SheetJS(this.data);
+    }; // reader.onload = (e: any) => {
+    reader.readAsBinaryString(target.files[0]);
+  } // onFileChange(evt: any)
+
+  processWorkbook(): void {
+    console.debug('processWorkbook: format is \'' + this.format + '\'');
+    this.wbText = '';
+    this.wbHtml = '';
+    if (this.format === 'CSV') {
+      this.wbText = this.sheetJS.workbook2csv(this.workbook);
+    } else if (this.format === 'JSON') {
+      this.wbText = this.sheetJS.workbook2json(this.workbook);
+    } else if (this.format === 'HTML') {
+      this.wbHtml = this.sheetJS.workbook2html(this.workbook);
+    }
+  } // processWorkbook(): void
+
+  saveAsXLSX(fileName: string): void {
+    /* generate worksheet */
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    /* save to file */
+    XLSX.writeFile(wb, fileName);
+  } // saveAsXLSX(fileName: string): void
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   ngOnInit() {
+    this.rABS = typeof FileReader !== 'undefined' && (FileReader.prototype || {}).readAsBinaryString;
+    this.domrabs = document.getElementsByName('userabs')[0];
+    if (!this.rABS) {
+      this.domrabs.disabled = !(this.domrabs.checked = false);
+    }
   }
 
+  setFormat(): void {
+
+  }
 }
